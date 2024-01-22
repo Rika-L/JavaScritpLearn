8d39d17e67db5db97cab4d2bacac699f37d4179b
Rika-L <2209393334@qq.com>
2024-01-22 14:22:22 +0800

    处理IE缓存问题：在请求网址后加上时间戳，一般用不上

--- AJAX尚硅谷/4-原生AJAX/server.js
+++ AJAX尚硅谷/4-原生AJAX/server.js
@@ -4,26 +4,26 @@ const express = require('express')
 const app = express()
 
 //3.创建路由规则
-app.get('/server', (request, response)=>{
+app.get('/server', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-control-Allow-Origin','*');
+    response.setHeader('Access-control-Allow-Origin', '*');
     //设置响应
     response.send('HELLO AJAX');
 });
 
 //可以接受任意类型的请求
-app.all('/server', (request, response)=>{
+app.all('/server', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-control-Allow-Origin','*');
+    response.setHeader('Access-control-Allow-Origin', '*');
     //响应头
     response.setHeader('Access-Control-Allow-Headers', '*');
     //设置响应
     response.send('HELLO AJAX POST');
 });
 
-app.all('/json-server', (request, response)=>{
+app.all('/json-server', (request, response) => {
     //设置响应头 设置允许跨域
-    response.setHeader('Access-control-Allow-Origin','*');
+    response.setHeader('Access-control-Allow-Origin', '*');
     //响应头
     response.setHeader('Access-Control-Allow-Headers', '*');
     //响应一个数据
@@ -34,8 +34,20 @@ app.all('/json-server', (request, response)=>{
     //设置响应
     response.send(str);
 });
-
+app.all('/ie', (request, response) => {
+    //设置响应头 设置允许跨域
+    response.setHeader('Access-control-Allow-Origin', '*');
+    //响应头
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    //响应一个数据
+    const data = {
+        name: 'Rika'
+    };
+    let str = JSON.stringify(data);
+    //设置响应
+    response.send(str);
+});
 //4.监听端口启动服务
-app.listen(8000, ()=>{
+app.listen(8000, () => {
     console.log('服务已经启动，8000端口监听中...');
 })

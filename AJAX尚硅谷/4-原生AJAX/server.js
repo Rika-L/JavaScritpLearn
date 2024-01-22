e40c187562bbd26187fe44e86a078d047e9d5212
Rika-L <2209393334@qq.com>
2024-01-22 10:37:56 +0800

    设置请求头

--- AJAX尚硅谷/4-原生AJAX/server.js
+++ AJAX尚硅谷/4-原生AJAX/server.js
@@ -11,9 +11,12 @@ app.get('/server', (request, response)=>{
     response.send('HELLO AJAX');
 });
 
-app.post('/server', (request, response)=>{
+//可以接受任意类型的请求
+app.all('/server', (request, response)=>{
     //设置响应头 设置允许跨域
     response.setHeader('Access-control-Allow-Origin','*');
+    //响应头
+    response.setHeader('Access-Control-Allow-Headers', '*');
     //设置响应
     response.send('HELLO AJAX POST');
 });
